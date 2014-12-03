c7bfb527bdcb2ab2c4f5f0808fb74d53db3091f2
Ghislain Loaec <gloaec@cadoles.com>
2014-12-03 02:06:32 +0100

    Change f*** cron update strategy

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,9 @@ require File.expand_path('../environment', __FILE__)
 upload_interval = Setting.for('Intervalle de rafraîchissement FTP (min)').value || 15
 
 every upload_interval.to_i.minutes do
-  rake 'whenever:update_crontab'
   runner 'Rapport.fetch'
+end
+
+every 1.minutes do
+  rake 'whenever:update_crontab'
 end
